59cf3b7081b8f8aa6524d100be40929b4615c565
Maus <maksimochka1_1@mail.ru>
2018-11-11 19:44:32 +0300

    me
    fix restore image and add some nothing

--- mrz1.py
+++ mrz1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from PIL import Image
 from time import ctime, time
 
@@ -44,26 +45,51 @@ def restore_image(
         output_hight + height - resize_h, output_weight + weight - resize_w, 3
     )
     # delete extra col from matrix, between -2 * weight and -weight
-    # mat = np.concatenate((mat[:, : -2 * weight + 1], mat[:, -weight:]), axis=1)
+    mat = np.concatenate((mat[:, : -2 * weight + resize_w], mat[:, -weight:]), axis=1)
     # delete extra col from matrix, between -2 * height and -height
-    # mat = np.concatenate((mat[: -2 * height + 1, :], mat[-height:, :]), axis=0)
+    mat = np.concatenate((mat[: -2 * height + resize_h, :], mat[-height:, :]), axis=0)
     img = Image.fromarray(mat.astype("uint8"), "RGB")
+    print(img.size)
     img.save(name, format="BMP")
 
 
-def start():
+def start(n=None, m=None, p=None, alpha=None, err=None, print_per_iter=None):
     """Take start arguments from user and runs learning alghoritm"""
-    n = int(input("Height of rectangle:\n"))
-    m = int(input("Weight of rectangle:\n"))
-    p = int(input("Number of neurons on second layer:\n"))
-    alpha = float(input("Alpha, enter 0 to auto select:\n"))
-
+    if n == None:
+        n = int(input("Height of rectangle:\n"))
+    if m == None:
+        m = int(input("Weight of rectangle:\n"))
+    if p == None:
+        p = int(input("Number of neurons on second layer:\n"))
+    if alpha == None:
+        alpha = float(input("Alpha, enter 0 to auto select:\n"))
+    if err == None:
+        err = int(input("Minimal error:\n"))
+    if print_per_iter == None:
+        print_per_iter = int(input("Number of iterations on which restore image:\n"))
     if alpha == 0.0:
-        alpha = 0.001877
+        alpha = 0.0005
     mat = prepare_image("256px-Lenna.png", n, m)
     # number of neurons in first and last layers
     N = n * m * 3
-    execute(mat, alpha, N, p, n, m, 256, 256)
+    L = mat.shape[0] * mat.shape[1]
+    z = (N * L) / ((N + L) * p + 2)
+    print("Z= {z}".format(z=z))
+    return [
+        z,
+        execute(
+            mat=mat,
+            alpha=alpha,
+            N=N,
+            p=p,
+            error_min=err,
+            print_per_iter=print_per_iter,
+            height=n,
+            weight=m,
+            output_height=256,
+            output_weight=256,
+        ),
+    ]
 
 
 def execute(
@@ -71,6 +97,8 @@ def execute(
     alpha: float,
     N: int,
     p: int,
+    error_min: int,
+    print_per_iter: int,
     height: int,
     weight: int,
     output_height: int,
@@ -86,67 +114,42 @@ def execute(
     while True:
         error_all = 0
         k += 1
-        x1 = np.ndarray((1, 0), dtype=float)
         time1 = time()
         for i in mat:
             for j in i:
-                # error = 0
-                # j = j.ravel()
-                # j = j.reshape((1, j.shape[0]))
                 y = np.matmul(j, w1)
                 x1 = np.matmul(y, w2)
                 dx = x1 - j
-                # error = (dx * dx).sum()
-                # error_all += error
                 w1 -= alpha * np.matmul(np.matmul(j.transpose(), dx), w2.transpose())
                 w2 -= alpha * np.matmul(y.transpose(), dx)
-                # break
-            # break
         for i in mat:
             for j in i:
-                error = 0
-                # j = j.ravel()
-                # j = j.reshape((1, j.shape[0]))
                 y = np.matmul(j, w1)
                 x1 = np.matmul(y, w2)
                 dx = x1 - j
                 error = (dx * dx).sum()
                 error_all += error
         time2 = time()
-        print("time for iteration ", time2 - time1)
-        # break
+        # print("time for iteration ", time2 - time1)
         print(k, " ", error_all)
-        if k % 10 == 0:
+        # if k != 0 and k % print_per_iter == 0:
+        #    res = []
+        #    for i in range(len(mat)):
+        #        row = []
+        #        for j in range(len(mat[i])):
+        #            row.append(np.matmul(np.matmul(mat[i, j], w1), w2))
+        #        res.append(row)
+        #    res = np.array(res)
+        #    restore_image(
+        #        "output" + str(k), res, height, weight, output_height, output_weight
+        #    )
+        if error_all < error_min:
+            return k
             res = []
             for i in range(len(mat)):
                 row = []
                 for j in range(len(mat[i])):
                     row.append(np.matmul(np.matmul(mat[i, j], w1), w2))
-                res.append(row)
-            res = np.array(res)
-            restore_image(
-                "output" + str(k), res, height, weight, output_height, output_weight
-            )
-        if error_all < 100:
-            res = []
-            for i in range(len(mat)):
-                row = []
-                for j in range(len(mat[i])):
-                    y = (
-                        (
-                            (
-                                2
-                                * mat[i, j]
-                                .ravel()
-                                .reshape(1, mat.shape[2] * mat.shape[3] * mat.shape[4])
-                                / 255
-                            )
-                            - 1
-                        )
-                        .dot(w1)
-                        .dot(w2)
-                    )
-                    row.append(y)
                 res.append(row)
             res = np.array(res)
             restore_image("output", res, height, weight, output_height, output_weight)
@@ -155,4 +158,10 @@ def execute(
 
 
 if __name__ == "__main__":
-    start()
+    result = []
+    result.append(start(n=8, m=8, p=44, alpha=0.00077, err=1000, print_per_iter=0))
+    # for i in np.arange(8, 86, 4):
+    #    result.append(start(n=8, m=8, p=i, alpha=0.0008, err=1000, print_per_iter=0))
+    df = pd.DataFrame(result)
+    df.to_csv("result1.scv")
+
